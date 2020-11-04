af0ddf1367d734a8421978e118ca5623b9a0e25d
littleboarx <mrdmanxx@gmail.com>
2020-11-04 21:35:07 +0800

    -fixbug

--- src/Hisotry/History.js
+++ src/Hisotry/History.js
@@ -13,38 +13,44 @@ export class History extends React.Component {
     }
     async componentDidMount() {
         this.props.setActive(1);
-        if(window.localStorage.getItem('history')!== null){
-            this.setState({    
-                historyLists:JSON.parse(window.localStorage.getItem('history'))
-            })}
+        if (window.localStorage.getItem('history') !== null) {
+            this.setState({
+                historyLists: JSON.parse(window.localStorage.getItem('history'))
+            })
         }
+    }
 
-    async handleDelete(id){
+    async handleDelete(id) {
         let temp = this.state.historyLists
-        for (let index in temp.lists){
-            if(temp.lists[index].id === id ){
-                temp.lists.splice(index,1)
+        for (let index in temp.lists) {
+            if (temp.lists[index].id === id) {
+                temp.lists.splice(index, 1)
             }
-            }
-            this.setState({historyLists:temp})
-            let ress = JSON.stringify(temp)
-            window.localStorage.setItem('history', ress)
+        }
+        this.setState({ historyLists: temp })
+        let ress = JSON.stringify(temp)
+        window.localStorage.setItem('history', ress)
     }
 
 
     render() {
         return <div style={{
-            flexGrow:'1',
+            display: 'flex',
+            flexDirection: 'column',
+            maxHeight: 'calc(100% - 49px)',
+            flexGrow: '1'
 
         }}>
-        <Title>我的记录</Title>
-        <div style= {{
-            flexGrow:'1',
-        }}>
-        {this.state.historyLists.lists.map((e, key) => {
-                return <HistoryList list={e} key={key} handleDelete = {this.handleDelete} />;
-            })}
-        </div>
+            <Title>我的记录</Title>
+            <div style={{
+                flexGrow: '1',
+                flexShrink: '1',
+                overflow: 'scroll',
+            }}>
+                {this.state.historyLists.lists.map((e, key) => {
+                    return <HistoryList list={e} key={key} handleDelete={this.handleDelete} />;
+                })}
+            </div>
         </div>;
     }
 }
